d2048601a374dbd75e2035e6dc31c13090b14769
tm276 <tm276@humboldt.edu>
2026-05-01 22:41:23 -0700

    Made a functioning connect with Nextcloud

--- app/src/main/java/com/example/timekeeper/ClientProfile.kt
+++ app/src/main/java/com/example/timekeeper/ClientProfile.kt
@@ -1,21 +1,4 @@
-package com.example.timekeeper
-
-data class ClientProfile(
-    val id: String,
-    val clientName: String,
-    val userName: String = "",
-    val csvFileName: String = "time_log.csv",
-
-    // Local storage
-    val localFolder: String = "",
-
-    // Google Drive
-    val googleDriveAccount: String = "",
-    val googleDriveFolder: String = "",
-
-    // Nextcloud
-    val nextcloudUrl: String = "",
-    val nextcloudUser: String = "",
-    val nextcloudPassword: String = "",
-    val nextcloudFolder: String = ""
-)
+// Modified: add fields
+// var autoSyncEnabled: Boolean
+// var syncGoogleDrive: Boolean
+// var syncNextcloud: Boolean
